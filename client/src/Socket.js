6d4cb27d9f8f2f7c3122542e1edcf3b18f7e7807
tandelmeghna763 <tandelmeghna763@gmail.com>
2025-07-05 12:22:02 +0530

    Update

--- client/src/Socket.js
+++ client/src/Socket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-const socket = io("https://your-server-name.onrender.com");
+const socket = io("https://socket-server-4lo3.onrender.com");
 
 export default socket;
